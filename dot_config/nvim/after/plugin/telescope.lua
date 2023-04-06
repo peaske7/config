3eb2698cc6ea716eeb4a6f57dcce1762d3663c06
DrPoppyseed <peaske16180@gmail.com>
2023-04-06 22:32:27 +0900

    find-files now respects gitignore

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -9,19 +9,11 @@ telescope.setup {
       n = {
         ['q'] = telescope_actions.close
       }
-    }
-  }
+    },
+  },
 }
 
-vim.keymap.set('n', '<leader>sf',
-  function()
-    telescope_builtin.find_files({
-      path = "%:p:h",
-      hidden = true
-    })
-  end,
-  { desc = '[S]earch [F]iles', noremap = true }
-)
+vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [F]iles', noremap = true })
 vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [H]elp', noremap = true })
 vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string, { desc = '[S]earch current [W]ord', noremap = true })
 vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [G]rep', noremap = true })
