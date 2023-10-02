73355606db76d56d29d740f6c312f6370228adbe
DrPoppyseed <peaske16180@gmail.com>
2023-10-02 20:58:49 +0900

    only hide gitignored files

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,13 @@
 local telescope_builtin = require('telescope.builtin')
 
+require('telescope').setup {
+  pickers = {
+    find_files = {
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+    },
+  },
+}
+
 local opts = { noremap = true }
 vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, opts)
 vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, opts)
