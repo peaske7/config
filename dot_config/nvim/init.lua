88c62e4d7169406dd748e9c7ee860d2dde865a2f
DrPoppyseed <peaske16180@gmail.com>
2023-12-13 15:13:26 +0900

    adds gen.nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -311,10 +311,31 @@ require('lazy').setup({
     end,
   },
 
+
+  {
+    "David-Kunz/gen.nvim",
+    opts = {
+      model = "mistral",
+      display_mode = "float",
+      show_prompt = false,   -- Shows the Prompt submitted to Ollama.
+      show_model = false,    -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = false, -- Never closes the window automatically.
+      init = function() pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
+      -- Function to initialize Ollama
+      command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
+      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+      -- This can also be a lua function returning a command string, with options as the input parameter.
+      -- The executed command must return a JSON object with { response, context }
+      -- (context property is optional).
+      list_models = '<function>', -- Retrieves a list of model names
+      debug = false               -- Prints errors and the command which is run.
+    }
+  },
+
   {
     -- Review PRs
     "ldelossa/gh.nvim",
-    lazy = true,
+    event = "BufReadPre",
     dependencies = {
       {
         "ldelossa/litee.nvim",
