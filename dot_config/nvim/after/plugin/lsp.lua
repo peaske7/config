041e1e1f34e81d87b1f463333c79aff6ea0b0144
DrPoppyseed <peaske16180@gmail.com>
2024-01-15 15:20:00 +0900

    migrate to rustaceanvim

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -45,17 +45,7 @@ require('mason-lspconfig').setup({
       require('lspconfig').lua_ls.setup(lua_opts)
     end,
     rust_analyzer = function()
-      local rust_tools = require('rust-tools')
-      rust_tools.setup({
-        server = {
-          on_attach = function(_, bufnr)
-            vim.keymap.set('n', '<leader>ca',
-              rust_tools.hover_actions.hover_actions,
-              { buffer = bufnr }
-            )
-          end
-        }
-      })
+      return true
     end,
   }
 })
--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -136,7 +136,44 @@ require('lazy').setup({
       'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-buffer',
-      'simrat39/rust-tools.nvim',
+      -- 'simrat39/rust-tools.nvim',
+
+      {
+        "mrcjkb/rustaceanvim",
+        version = "^3",
+        ft = { "rust" },
+        opts = {
+          settings = {
+            -- rust-analyzer language server configuration
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
+              -- Add clippy lints for Rust.
+              checkOnSave = {
+                allFeatures = true,
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
+              },
+            },
+          },
+        },
+        config = function(_, opts)
+          vim.g.rustaceanvim = vim.tbl_deep_extend("force",
+            {},
+            opts or {})
+        end
+      },
 
       {
         "L3MON4D3/LuaSnip",
