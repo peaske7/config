26a36efb0ccec8039a7fe7aac2d7fe3e367528ad
DrPoppyseed <peaske16180@gmail.com>
2024-03-25 00:12:02 +0900

    nvim-highlight

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -284,6 +284,19 @@ require('lazy').setup({
   },
 
   {
+    'brenoprata10/nvim-highlight-colors',
+    event = "BufReadPre",
+    config = function()
+      require('nvim-highlight-colors').setup({
+        render = 'background',
+        virtual_symbol = '■',
+        enable_named_colors = true,
+        enable_tailwind = false,
+      })
+    end
+  },
+
+  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     opts = {}
